8c5408c0b915c63dcdb8d1875b7fdb1a220a8793
denisrogovoy <denis@knu.ua>
2020-06-21 16:39:52 +0300

    fixed

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+#Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+user = Spree::User.new
+user.email = 'email'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.spree_roles << Spree::Role.find_or_create_by(name: "admin")
+user.save!
+
+
+
